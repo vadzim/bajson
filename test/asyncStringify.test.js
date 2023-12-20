8d7d179bc802d0128c19fe4084c0e0888f00b8c3
Vadzim Zieńka <v@vadzim.info>
2023-12-20 09:39:33 +0100

    move performance test to another file

--- test/asyncStringify.test.js
+++ test/asyncStringify.test.js
@@ -266,33 +266,3 @@ await describe("main", async () => {
 		})
 	}
 })
-
-const time = async func => {
-	const start = process.cpuUsage()
-	const result = await func()
-	const stop = process.cpuUsage(start)
-	const duration = stop.user + stop.system
-	return [duration / 1_000_000, result]
-}
-
-await test("performance", async () => {
-	const [dataDuration, data] = await time(() =>
-		[...new Array(300)].map((_, index) =>
-			Object.fromEntries(
-				[...new Array(100)].map((_, index) => [
-					"c" + index,
-					[...new Array(100)].map((_, index) => ["b" + index, [Math.random(), Math.random(), Math.random()]]),
-				]),
-			),
-		),
-	)
-	const [jsonDuration, json] = await time(() => JSON.stringify(data))
-	const [textDuration, textSize] = await time(async () => {
-		let size = 0
-		for await (const chunk of stringify(data)) size += chunk.length
-		return size
-	})
-	if (textDuration > jsonDuration * 5) {
-		assert.equal({ textDuration, jsonDuration }, {})
-	}
-})
