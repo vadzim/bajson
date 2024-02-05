845a1d242eb3d8eff52958458d92d2be66e1b8bc
Vadzim Zieńka <v@vadzim.info>
2024-02-05 15:51:04 +0100

    change interface

--- src/asyncParse.d.ts
+++ src/asyncParse.d.ts
@@ -1,30 +1,42 @@
-export type ParsedJSON<Key extends string | number | undefined = string | number | undefined> = {
-	key: Key
-} & (
-	| {
-			type: "null"
-			value: Promise<null>
-	  }
-	| {
-			type: "number"
-			value: Promise<number>
-	  }
-	| {
-			type: "boolean"
-			value: Promise<boolean>
-	  }
-	| {
-			type: "string"
-			value: Promise<string> & AsyncIterable<Uint8Array>
-	  }
-	| {
-			type: "object"
-			value: Promise<Record<string, unknown>> & AsyncIterable<ParsedJSON<string>>
-	  }
-	| {
-			type: "array"
-			value: Promise<unknown[]> & AsyncIterable<ParsedJSON<number>>
-	  }
-)
+// TODO: is it possible to integrate with io-ts to emit typed values?
 
-export declare function parse(data: AsyncIterable<Uint8Array>): Promise<ParsedJSON<undefined>>
+export type ParsedJSON = ParsedNull | ParsedBoolean | ParsedNumber | ParsedString | ParseMap | ParsedArray
+
+type ParsedNull = {
+	type: "null"
+	read: () => Promise<null>
+}
+
+type ParsedBoolean = {
+	type: "boolean"
+	read: () => Promise<boolean>
+}
+
+type ParsedNumber = {
+	type: "number"
+	read: () => Promise<number>
+}
+
+type ParsedString = {
+	type: "string"
+	read: () => Promise<string>
+	stream: () => AsyncIterable<Uint8Array>
+}
+
+type ParseMap<T = unknown, P = ParsedJSON> = {
+	type: "map"
+	read: () => Promise<Record<string, T>>
+	entries: () => AsyncIterable<[key: string, value: P]>
+	keys: () => AsyncIterable<string>
+	values: () => AsyncIterable<P>
+}
+
+type ParsedArray<T = unknown, P = ParsedJSON> = {
+	type: "array"
+	read: () => Promise<T[]>
+	entries: () => AsyncIterable<[key: number, value: P]>
+	keys: () => AsyncIterable<number>
+	values: () => AsyncIterable<P>
+}
+
+export declare function parse(data: AsyncIterable<Uint8Array>): Promise<ParsedJSON>
